7c3525bf709561bc397e3cdf814565ee55ee4b5b
feng0726 <815930656@qq.com>
2020-12-02 14:58:53 +0800

    test.java 修改

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public String sayHello（String content）｛
 System.out.println（content）;
 ｝
  public static void main(String[] str){
- new Test().sayHello（"hello github!!!"）;
+ new Test().sayHello（"hello github  哈哈哈!!!"）;
+ new Test().sayHello（"hello github  hahaha!!!"）;
  }
 ｝
